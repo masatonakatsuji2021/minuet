99e720f4c26a270b9cb806a13597f6c6864e68ba
masato nakatsuji <nakatsuji.sak19@gmail.com>
2024-05-20 21:58:26 +0900

    5/20

--- core/construct.js
+++ core/construct.js
@@ -2,6 +2,8 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 class Construct {
 }
+Construct.user = "minuet";
+Construct.group = "minuet";
 Construct.rootDir = "/var/minuet";
 Construct.tempDir = Construct.rootDir + "/.temp";
 Construct.workPidPath = Construct.tempDir + "/.work";
--- core/construct.ts
+++ core/construct.ts
@@ -1,5 +1,9 @@
 export default class Construct {
 
+    public static user : string = "minuet";
+
+    public static group : string = "minuet";
+
     public static rootDir : string = "/var/minuet";
 
     public static tempDir : string = Construct.rootDir + "/.temp";
